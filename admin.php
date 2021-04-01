8d8eeed1a19584ff45d775aceb4790ae46da5199
daxi001 <atavkhelidze@daxi.ge>
2021-04-01 17:37:57 +0400

    new job 01.04.21

--- admin.php
+++ admin.php
@@ -15,7 +15,7 @@
         <div class="dropdown">
             <button onclick="myFunction()" class="dropbtn"><span class="material-icons NavMenuSpan" onclick="myFunction()">more_vert</span></button>
             <div id="myDropdown" class="dropdown-content">
-                <a class="addCategoryBtn" href="#home"  data-bs-toggle="modal" data-bs-target=".addCategoryModal" data-bs-whatever="@addCategory"><span class="material-icons logoutIcon">add</span>&nbsp;კატეგორიის დამატება</a>
+                <a class="addCategoryBtn" href="#home"  data-bs-toggle="modal" data-bs-target="#showaddModal"><span class="material-icons logoutIcon">add</span>&nbsp;მეთოდები&nbsp;კატეგორიებზე</a>
                 <!-- <a href="#about" data-bs-toggle="modal" data-bs-target=".addProductModal" data-bs-whatever="@addProduct"><span class="material-icons logoutIcon">add</span>&nbsp;პროდუქტის დამატება</a> -->
                 <a href="#contact"><span class="material-icons logoutIcon">logout</span>&nbsp;გასვლა</a>
             </div>
@@ -48,49 +48,47 @@
 
 </div>
 
-<div class="modal fade scale-up-center addCategoryModal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- კატეგორიის დამატების მოდალი -->
+<div class="modal fade scale-up-center addCategoryModal" id="showaddModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
     <div class="modal-content">
       <div class="modal-header" style="color: white;">
-        <h5 class="modal-title" id="exampleModalLabel"></h5>
+        <h5 class="modal-title" id="exampleModalLabel">მეთოდები კატეგორიებზე</h5>
         <span class="material-icons" data-bs-dismiss="modal" aria-label="Close" style="color: white; cursor: pointer;">close</span>
       </div>
-      <!-- <form class="addCategory"> -->
       <div class="modal-body" >
         <div class="addDiv" style="display: flex; align-items: center;">
             
         </div>
       </div>
-      <!-- <div class="modal-footer">
-        <button type="submit" class="btn btn-primary SaveBtn" data-bs-dismiss="modal" id="liveToastBtn">შენახვა</button>
-      </div> -->
+    </div>
+  </div>
+</div>
+
+<!-- კატეგოტიებზე მეთოდი -->
+<div class="modal fade scale-up-center CategoryModal" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content" style="box-shadow: 0 0px 10px rgb(0 0 0 / 30%);">
+      <div class="modal-header" style="background-color: #2d69a7; color: white;">
+        <h5 class="modal-title" id="exampleModalLabel2"></h5>
+        <span class="material-icons" data-bs-dismiss="modal" aria-label="Close" style="color: white; cursor: pointer;">close</span>
+      </div>
+      <!-- <form class="methodCategory"> -->
+        <div class="modal-body" >
+          <div class="CategoryInfo" style="display: flex; align-items: center;">
+              
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button class="btn btn-primary SaveBtn" data-bs-dismiss="modal" id="liveToastBtn">შენახვა</button>
+        </div>
       <!-- </form> -->
     </div>
   </div>
 </div>
 
-<!-- <div class="modal fade scale-up-center CategoryModal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header" style="background-color: #2d69a7; color: white;">
-        <h5 class="modal-title" id="exampleModalLabel"></h5>
-        <span class="material-icons" data-bs-dismiss="modal" aria-label="Close" style="color: white; cursor: pointer;">close</span>
-      </div>
-      <form class="addCategory">
-      <div class="modal-body" >
-        <div class="addDiv" style="display: flex; align-items: center;">
-            
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="submit" class="btn btn-primary SaveBtn" data-bs-dismiss="modal" id="liveToastBtn">შენახვა</button>
-      </div>
-      </form>
-    </div>
-  </div>
-</div> -->
-
-<div class="modal fade scale-up-center addProductModal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- კპროდუქტის დამატების მოდალი -->
+<div class="modal fade scale-up-center addProductModal" id="addProductModalId" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-xl modal-dialog-centered">
     <div class="modal-content" style="border-radius:10px;">
       <div class="modal-header" style="background-color: #2d69a7; color: white;">
@@ -122,11 +120,7 @@
                           
               </div>  
           </div>
-              <!-- <div class="upload-btn imgUpload" onclick="selectFile()">
-                  <i class="fas fa-cloud-upload-alt"></i>
-                  <span>Choose files to Upload</span>
-                  <input type="file" id="upload-file" onchange="uploads(event)" multiple="" accept="image/*">
-              </div> -->
+
             </div>
           </div>
       </div>
@@ -198,7 +192,7 @@ const dragArea = document.querySelector(".wrapper");
 });
 
 
-var exampleModal = document.getElementById('exampleModal')
+var exampleModal = document.getElementById('addProductModalId')
 exampleModal.addEventListener('show.bs.modal', function (event) {
   // Button that triggered the modal
   var button = event.relatedTarget
@@ -207,7 +201,7 @@ exampleModal.addEventListener('show.bs.modal', function (event) {
 
     if(recipient == '@addCategory'){
 
-      $(".addDiv").attr('data-bs-whatever',recipient);
+      $(".addDiv").attr('data-bs-whatever');
 
       $('#exampleModalLabel').text('კატეგორიის დამატება');
     }
